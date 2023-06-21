71416eb3f412d6a6f115d486c498eed7b57891fc
Nad GarDez <iranad@hotmail.com>
2023-06-21 17:11:39 -0400

    Initial Commit

---
+++ custom-video/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  custom-video
+//
+//  Created by Nad on 21/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
